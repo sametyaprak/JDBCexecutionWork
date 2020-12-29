2c97381f2e97a1bfc8c6ab6c520936888cf30b0b
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2020-12-29 20:51:10 +0100

    test

--- src/test/java/tests/US_01_Test.java
+++ src/test/java/tests/US_01_Test.java
@@ -2,18 +2,42 @@ package tests;
 import static io.restassured.RestAssured.*;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import utilities.ConfigReader;
 
 public class US_01_Test {
     Response response;
     String endPoint = "https://gorest.co.in/public-api/users/";
+    JsonPath json;
+
+    public void getResponse(){
+        response = given().accept(ContentType.JSON).auth().oauth2(ConfigReader.getProperty("token")).when().get(endPoint);
+    }
 
     @Test
     public void tc0101(){
-        response = given().accept(ContentType.JSON).auth().oauth2(ConfigReader.getProperty("token")).when().get(endPoint);
-        response.prettyPrint();
+        getResponse();
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+    @Test
+    public void tc0102(){
+        getResponse();
+        Assert.assertEquals(response.getContentType(),"application/json; charset=utf-8");
+    }
+    @Test
+    public void tc0103(){
+        getResponse();
+        json = response.jsonPath();
+        Assert.assertEquals(json.getInt("meta.pagination.total"),1903);
+    }
+    @Test
+    public void tc0104(){
+        getResponse();
+        json = response.jsonPath();
+        Assert.assertEquals(json.getInt("meta.pagination.total"),1903);
     }
 }
