9a55f4667fed126aeefb436b4a0a70bb39dcdb1f
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2021-01-11 03:03:54 +0100

    test

--- src/test/java/tests/US_01_Test_GetTest.java
+++ src/test/java/tests/US_01_Test_GetTest.java
@@ -95,7 +95,7 @@ public class US_01_Test_GetTest {
         Assert.assertTrue(sum==0);
     }
     @Test
-    public void tc106(){
+    public void tc0106(){
         getResponse(endPoint);
         json = response.jsonPath();
         int totalPages = json.getInt("meta.pagination.pages");
