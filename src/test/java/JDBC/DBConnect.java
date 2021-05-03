18e2082d15604d91d6eee5a33c827472c766fd09
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2021-05-03 02:46:00 +0200

    jdbc workk

---
+++ src/test/java/JDBC/DBConnect.java
@@ -0,0 +1,61 @@
+package JDBC;
+
+import org.testng.annotations.Test;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBConnect {
+
+    Connection connection;
+    Statement statement;
+
+    PreparedStatement preparedStatement;
+    CallableStatement callableStatement;
+
+    ResultSet resultSet;
+    ResultSetMetaData resultSetMetaData;
+
+    @Test
+    void simpleQuery() throws SQLException {
+        //once database baglantisi yapilir. get connection 3 argument alir. url-user-password
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/","postgres", "Ankara06");
+        //olusturulan connection uzerinden Statement
+        statement = connection.createStatement();
+        /*
+        statement.execute();
+        statement.executeQuery();
+        statement.executeUpdate();
+        */
+        resultSet = statement.executeQuery("select city from customers;");
+
+        List<String> myData = new ArrayList<>();
+
+        while (resultSet.next())
+            //myData.add(resultSet.getString("city"));
+            myData.add(resultSet.getString(1));
+
+        System.out.println("myData = " + myData);
+    }
+    @Test
+    void update() throws SQLException {
+        //once database baglantisi yapilir. get connection 3 argument alir. url-user-password
+        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/","postgres", "Ankara06");
+
+        statement = connection.createStatement();
+        /*
+        statement.executeUpdate();
+        */
+        int row = statement.executeUpdate("update customers\n" +
+                                            "set city = 'no data'\n" +
+                                            "where city is null;");
+        System.out.println(row + " rows updated");
+
+    }
+
+
+
+
+
+}
--- src/test/java/databaseWork/Ders1.java
+++ src/test/java/databaseWork/Ders1.java
@@ -7,6 +7,7 @@ import java.sql.*;
 import java.util.*;
 
 public class Ders1 {
+
     ResultSet resultSet;
     String queryTest1 = "select \"public\".products.category_id, count (category_id) as total_product\n" +
             "from \"public\".products\n" +
@@ -17,7 +18,12 @@ public class Ders1 {
             "join \"public\".products \n" +
             "on \"public\".categories.category_id = \"public\".products.category_id\n" +
             "where category_name = 'Seafood'";
+    String queryTest4 = "insert into customers (contact_name,company_name,customer_id)\n" +
+            "(select first_name, title, employee_id from em" + "ployees ORDER BY random() LIMIT 1)";
+
+    String queryTest4Control = "select contact_name,company_name,customer_id from customers";
     List<Map<String,String>> dataList = new ArrayList<>();
+    List<Map<String,String>> dataList2 = new ArrayList<>();
 
     @Test
     public void test1() throws SQLException {
@@ -34,4 +40,15 @@ public class Ders1 {
         dataList = DatabaseConnectorV1.getQueryResultWithAListMap(queryTest3);
         Assert.assertEquals(dataList.size(),12);
     }
+    @Test
+    public void test4() throws SQLException {
+        dataList = DatabaseConnectorV1.getQueryResultWithAListMap(queryTest4);
+        dataList2 = DatabaseConnectorV1.getQueryResultWithAListMap(queryTest4Control);
+        long number = dataList2.stream()
+                .filter(x->x.get("contact_name")
+                .equals(dataList.get(0)
+                .get("first_name"))).count();
+        System.out.printf("", number);
+    }
+
 }
