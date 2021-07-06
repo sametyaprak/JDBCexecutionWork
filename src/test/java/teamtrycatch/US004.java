759a043d432d375057110ec51222525f8a179ed8
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2021-07-06 23:58:49 +0200

    us004

--- src/test/java/teamtrycatch/US004.java
+++ src/test/java/teamtrycatch/US004.java
@@ -1,28 +1,42 @@
 package teamtrycatch;
 
+import org.junit.Assert;
 import org.junit.Test;
+import utilities.DatabaseConnectorV1;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.SQLException;
 
 public class US004 {
 
-    String tc0301Query = "";
-    String tc0302QueryUpdate = "";
-    String tc0302Query = "";
-    String tc0303Query = "";
+    String tc0401Query = "select round(avg(maas),2)from personel";
+    String tc0402Query = "select count(calisan_id) from personel";
+    String tc0403Query = "select max(maas)-min(maas) from personel";
 
     ResultSet resultSet;
-    List<Object> myAllData = new ArrayList<>();
 
     @Test
-    public void tc0401() {
+    public void tc0401() throws SQLException {
+        resultSet = DatabaseConnectorV1.executeQuery(tc0401Query);
+        while (resultSet.next()){
+            double a = (resultSet.getDouble(1));
+            Assert.assertEquals(2266.21, a);
+        }
     }
     @Test
-    public void tc0402() {
+    public void tc0402() throws SQLException {
+        resultSet = DatabaseConnectorV1.executeQuery(tc0402Query);
+        while (resultSet.next()){
+            int a = (resultSet.getInt(1));
+            Assert.assertEquals(29, a);
+        }
     }
     @Test
-    public void tc0403() {
+    public void tc0403() throws SQLException {
+        resultSet = DatabaseConnectorV1.executeQuery(tc0403Query);
+        while (resultSet.next()){
+            int a = (resultSet.getInt(1));
+            Assert.assertEquals(3060, a);
+        }
     }
 
 
